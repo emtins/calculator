8cf8b7f64e139a1c3dfcd9a07ab14e45c0c535e3
emtins <lilylynch77@gmail.com>
2024-05-27 19:56:46 +0300

    Додавання у файл Calculator.cpp функцій: додавання, віднімання, множення.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,1 +1,13 @@
 #include "Calculator.h"
+
+double Calculator::addition(double a, double b) {
+    return a + b;
+}
+
+double Calculator::subtraction(double a, double b) {
+    return a - b;
+}
+
+double Calculator::multiplication(double a, double b) {
+    return a * b;
+}
