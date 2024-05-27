f0f74d1a0ea0ccd8b8ef26e2aba767179fe64545
emtins <lilylynch77@gmail.com>
2024-05-27 20:04:35 +0300

    Додавання препроцесорної команди #include <stdexcept> для обробки виключень; додавання функції ділення, з урахуванням виключення invalid_argument у разі ділення на нуль

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,4 +1,6 @@
 #include "Calculator.h"
+#include <stdexcept>
+using namespace std;
 
 double Calculator::addition(double a, double b) {
     return a + b;
@@ -10,4 +12,10 @@ double Calculator::subtraction(double a, double b) {
 
 double Calculator::multiplication(double a, double b) {
     return a * b;
+}
+double Calculator::division(double a, double b) {
+    if (b == 0) {
+        throw invalid_argument("You cannot divide by 0");
+    }
+    return a / b;
 }
